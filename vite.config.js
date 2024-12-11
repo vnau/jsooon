69d795c7a7c4ab1fb8ee1fb4afa1a82549864da8
vnau <20983143+vnau@users.noreply.github.com>
2024-12-11 22:37:40 +0100

    add test workflow

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
-  },
-})
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    }
+  }
+});
